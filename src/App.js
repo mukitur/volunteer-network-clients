aafda199761d5691d22627f384a756e202616384
mukitur <mukit.inway@gmail.com>
2021-10-27 21:32:19 +0600

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Donation from './Pages/Donation/Donation';
+import Events from './Pages/Events/Events';
+import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path = "/">
+            <Home></Home>
+          </Route>
+          <Route path = "/home">
+            <Home></Home>
+          </Route>
+          <Route path="/events">
+              <Events></Events>
+          </Route>
+          <Route path ="/donation">
+              <Donation></Donation>
+          </Route>
+          <Route path="/blogs">
+              <Blogs></Blogs>
+          </Route>
+          <Route path="*">
+              <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, Button } from 'react-router-dom';
+import logo from '../../images/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand as = {Link} to="/"><img src={logo} width="100%" height="60" alt=""/></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto fs-5">
+                        <Nav.Link as = {Link} to ="/home">Home</Nav.Link>
+                        <Nav.Link as = {Link} to ="/events">Events</Nav.Link>
+                        <Nav.Link as = {Link} to ="/donation">Donation</Nav.Link>
+                        <Nav.Link as = {Link} to ="/blogs">Bolgs</Nav.Link>
+                    </Nav>
+                    <Nav className="fs-6">
+                       {/*  {
+                            user?.email ?
+                            <Button className="me-2" onClick={logout} variant="light">LogOut</Button> :
+                            <Nav.Link as = {Link} eventKey={2} to="/login">Login/Register</Nav.Link>
+                        } */}
+                        <Navbar.Text>Signed in as: <a href="#login">{/* {user?.displayName} */}</a></Navbar.Text>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </div>
+    );
+};
+
+export default Header;
